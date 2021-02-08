cb9f48f880efd2cfd6affcfd684519106cbc63ce
max <sumeth.sir@student.mahidol.edu>
2021-02-08 12:11:32 +0700

    add vue-router

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import '@babel/polyfill'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
